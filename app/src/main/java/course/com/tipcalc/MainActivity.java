9156ba9caf1ced4d09511e51ff0172e8325c4a20
Marcos Mercuri <marcosmercuri@packlink.es>
2016-09-30 23:38:58 +0200

    Bind the view elements and handle the submit click

--- app/src/main/java/course/com/tipcalc/MainActivity.java
+++ app/src/main/java/course/com/tipcalc/MainActivity.java
@@ -1,18 +1,46 @@
 package course.com.tipcalc;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int TIP_STEP_CHANGE = 1;
+    private static final int DEFAULT_TIP_PERCENTAGE = 10;
+
+    @Bind(R.id.inputBill)
+    EditText inputBill;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    @Bind(R.id.inputPercentage)
+    EditText inputPercentage;
+    @Bind(R.id.btnIncrease)
+    Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    Button btnDecrease;
+    @Bind(R.id.btnClear)
+    Button btnClear;
+    @Bind(R.id.txtTip)
+    TextView txtTip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -27,6 +55,24 @@ public class MainActivity extends AppCompatActivity {
             about();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.btnSubmit)
+    public void handleClickSubmit() {
+        Log.d(getLocalClassName(), "Click in submit");
+        hideKeyboard();
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager intputManager =
+                (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        try {
+            intputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (NullPointerException npe) {
+            Log.e(getLocalClassName(), Log.getStackTraceString(npe));
+        }
     }
 
     private void about() {
