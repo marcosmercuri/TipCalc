dd54b58e37c78628e60862f1c712f8928f8a93fd
Marcos Mercuri <marcosmercuri@gmail.com>
2016-09-30 23:09:41 +0200

    Add 'about' menu item

--- app/src/main/java/course/com/tipcalc/MainActivity.java
+++ app/src/main/java/course/com/tipcalc/MainActivity.java
@@ -1,7 +1,11 @@
 package course.com.tipcalc;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_about) {
+            about();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void about() {
+        Intent openBrowserIntent = new Intent(Intent.ACTION_VIEW);
+        openBrowserIntent.setData(Uri.parse(TipCalcApplication.ABOUT_URL));
+        startActivity(openBrowserIntent);
     }
 }
---
+++ app/src/main/java/course/com/tipcalc/TipCalcApplication.java
@@ -0,0 +1,7 @@
+package course.com.tipcalc;
+
+import android.app.Application;
+
+public class TipCalcApplication extends Application {
+    public static final String ABOUT_URL = "https://github.com/marcosmercuri/TipCalc";
+}
