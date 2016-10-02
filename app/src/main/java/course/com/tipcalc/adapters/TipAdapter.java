d46190497e7fa728a8d6253b0bc0ef1dfdb74e27
Marcos Mercuri <marcosmercuri@packlink.es>
2016-10-02 15:10:10 +0200

    Add tipRecord and item_row

---
+++ app/src/main/java/course/com/tipcalc/adapters/TipAdapter.java
@@ -0,0 +1,66 @@
+package course.com.tipcalc.adapters;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import course.com.tipcalc.R;
+import course.com.tipcalc.model.TipRecord;
+
+public class TipAdapter extends RecyclerView.Adapter<TipAdapter.ViewHolder>  {
+    private List<TipRecord> dataset;
+    private Context context;
+
+    public TipAdapter(List<TipRecord> dataset, Context context) {
+        this.dataset = dataset;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_row, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        TipRecord element = dataset.get(position);
+        String tip = String.format(context.getString(R.string.global_message_tip), element.getTip());
+        holder.txtContent.setText(tip);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
+
+    public void add(TipRecord record) {
+        dataset.set(0, record);
+        notifyDataSetChanged();
+    }
+
+    public void clear() {
+        dataset.clear();
+        notifyDataSetChanged();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.txtContent)
+        TextView txtContent;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
---
+++ app/src/main/java/course/com/tipcalc/model/TipRecord.java
@@ -0,0 +1,44 @@
+package course.com.tipcalc.model;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TipRecord {
+    private double bill;
+    private int tipPercentage;
+    private Date createdDate;
+
+    public double getBill() {
+        return bill;
+    }
+
+    public void setBill(double bill) {
+        this.bill = bill;
+    }
+
+    public int getTipPercentage() {
+        return tipPercentage;
+    }
+
+    public void setTipPercentage(int tipPercentage) {
+        this.tipPercentage = tipPercentage;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public double getTip() {
+        return  bill * (tipPercentage / 100);
+    }
+
+    public String getFormattedCreatedDate() {
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyyy HH:mm");
+        return format.format(createdDate);
+    }
+}
